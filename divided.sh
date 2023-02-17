ed1c6216c0190e5fb4384f2907b20b7cb93c38d2
Ellen Hoffman <71892256+ehoffmanlab@users.noreply.github.com>
2023-02-16 22:44:22 -0500

    Delete divided.sh

--- divided.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for file in `ls r_*gz | grep -v div | sed 's/_C.__optfixed.nii.gz//g' | uniq`
-do
-	echo ${file}
-	fslmaths ${file}_C0__optfixed.nii.gz -div ${file}_C1__optfixed.nii.gz ${file}_C0divC1.nii.gz
-done
-
